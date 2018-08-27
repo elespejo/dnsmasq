2455a369f3bcbd03e8edc37e5bc71641d7826590
Valerio-Perez <Valerio.Perez@protonmail.com>
2018-08-27 15:26:57 +0800

    update check_status.sh : check status in service

--- check_status.sh
+++ check_status.sh
@@ -1,17 +1,41 @@
 #!/bin/bash
 
-echo "Check docker compose :"
+function Hint() {
+    hint="$1"
+
+    Green="\033[0;32m"
+    NC="\033[0m"
+    printf "${Green}= ${NC}%.0s" {1..20}
+    echo -e "\n${Green}${hint}${NC}"
+    printf "${Green}= ${NC}%.0s" {1..20}
+    echo -e "\n"  
+}
+
+Hint "Check docker compose :"
 docker-compose ps 
 
-echo -e "\nCheck docker container :"
-docker ps -a | grep dnsmasq-x86_router_dnsmasq_1
+Hint "Check docker container :"
+docker ps -a | grep router_dnsmasq
 
-echo -e "\nCheck docker logs :"
-docker logs dnsmasq-x86_router_dnsmasq_1
+Hint "Check docker logs :"
+docker-compose logs
 
-echo -e "\nCheck netstat :"
-sudo netstat -tunpl | grep docker-proxy
+Hint "Check netstat :"
+sudo netstat -tunpl | grep dnsmasq
 
-echo -e "\nResolve baidu and youtube :"
+Hint "Resolve baidu and youtube :"
 nslookup www.baidu.com 127.0.0.1
 nslookup www.youtube.com 127.0.0.1
+
+Hint "Check script init in container :"
+docker-compose exec router_dnsmasq ls -l init
+
+Hint "Check /var/cache/apk/ empty in container :"
+docker-compose exec router_dnsmasq ls -l /var/cache/apk/
+
+Hint "Check /etc/dnsmasq and /etc/dnsmasq.d exist in container :"
+docker-compose exec router_dnsmasq ls -l /etc/dnsmasq
+docker-compose exec router_dnsmasq ls -l /etc/dnsmasq.d
+
+Hint "Check process dnsmasq -k -C /etc/dnsmasq/dnsmasq.conf in container :"
+docker-compose exec router_dnsmasq ps aux
