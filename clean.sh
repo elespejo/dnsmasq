b25316e7ef8456a8189cfaaf78c68a7c4f82ae39
Valerio-Perez <Valerio.Perez@protonmail.com>
2018-09-03 10:16:00 +0800

    update clean.sh : 1. remove log in dnsmasq_log 2. checkout dnsmasq_log

--- clean.sh
+++ clean.sh
@@ -2,9 +2,12 @@
 
 docker-compose down
 
-for i in check_status.sh dnsmasq_conf docker-compose.yml Dockerfile init README.md
+rm dnsmasq_log/*
+
+for i in check_status.sh dnsmasq_conf dnsmasq_log docker-compose.yml Dockerfile init README.md
 do
     git checkout $i
 done
 
 docker rmi meninasx86/dnsmasq-x86:test
+
