107f9a979cb594655ebf7e094f518cb5f763c973
Valerio-Perez <Valerio.Perez@protonmail.com>
2018-09-17 15:27:50 +0800

    add deploy.sh to push image to docker hub

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+USER=$1
+PASS=$2
+ARCH=$3
+TAG=$4
+docker login -u $USER -p $PASS
+docker tag valerioperez/dnsmasq valerioperez/dnsmasq:$ARCH-$TAG
+docker push valerioperez/bypass:$ARCH-$TAG
