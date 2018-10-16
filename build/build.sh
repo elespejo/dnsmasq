8632abe21153192ccdd87d43225944baaaf71661
Valerio-Perez <Valerio.Perez@protonmail.com>
2018-10-16 11:56:59 +0800

    update image structure: 1. rename build to image 2. remove scripts build.sh and deploy.sh

--- build/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curdir=`cd $(dirname $0);pwd`
-arch="$1"
-
-docker build -f $curdir/Dockerfile-$arch -t elespejo/dnsmasq-$arch $curdir
--- build/deploy.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-USER=$1
-PASS=$2
-ARCH=$3
-TAG=$4
-docker login -u $USER -p $PASS
-docker tag elespejo/dnsmasq-$ARCH elespejo/dnsmasq-$ARCH:$TAG
-docker push elespejo/dnsmasq-$ARCH:$TAG
