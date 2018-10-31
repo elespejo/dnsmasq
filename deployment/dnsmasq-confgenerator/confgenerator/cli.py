68f2df5d74953949cf7eca29cddbe75a2b455ff1
Valerio-Perez <Valerio.Perez@protonmail.com>
2018-10-31 14:19:49 +0800

    Update confgenerator: 1. move accelerated-domains.china.conf to confgenerator 2. generate dns-config.env in specified path

--- deployment/dnsmasq-confgenerator/confgenerator/cli.py
+++ deployment/dnsmasq-confgenerator/confgenerator/cli.py
@@ -2,6 +2,7 @@
 
 import os
 import yaml
+from shutil import copy
 import argparse
 import confgenerator.generator.generator as gen
 
@@ -9,17 +10,20 @@ pwd = os.path.dirname(os.path.realpath(__file__))
 
 parser = argparse.ArgumentParser(description='Generate the dnsmasq configuration.')
 parser.add_argument('conf-info')
-parser.add_argument('-d', '--destination', dest='path', default=pwd+'/../dnsmasq_conf')
+parser.add_argument('-d', '--destination', dest='path', default=pwd+'/../dnsmasq')
 
 args = vars(parser.parse_args())
 info = args['conf-info']
 path = args['path']
 
+os.makedirs(path+'/dnsmasq_conf')
+copy(pwd+'/accelerated-domains.china.conf', path+'/dnsmasq_conf')
+
 info_dict = yaml.load(open(info, 'r'))
 for key, val in info_dict.items():
     generator = getattr(gen, "Gen_"+key.replace('-', '_'))
-    generator(val, path).write()
+    generator(val, path+'/dnsmasq_conf').write()
 
-f = open(pwd+'/../config.env', 'w')
-f.write("CONF_DIR="+pwd+'/../dnsmasq_conf')
+f = open(path+'/dns-config.env', 'w')
+f.write("CONF_DIR="+path+'/dnsmasq_conf')
 f.close
